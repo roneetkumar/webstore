ff1f74333e0b402a261a33ac70a172460a51ff0b
Roneet <a4all.a4aryan@gmail.com>
2016-12-29 20:55:28 +0530

    svg's, search bar, nav bar, overlay, fab, rotation, scrolling chips,
    every thing mentioned above

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,36 @@
 $(function() {
-    //hide elements on start
-    $('.overlay, h1.fab-title, .icon-close, .form-wrapper, svg.done').hide();
+    //hide-elements-on-start-------------------------------------
+    $('.overlay, h1.fab-title, .close-btn, .form-wrapper, svg.done-btn').hide();
 
-    //fab open function
+    //fab-open-function------------------------------------------
     function fabOpen() {
         $('.upload-btn').fadeOut(350)
         $('.fab').addClass('fab-open');
         $('.overlay').fadeIn(700);
         $('.fab-header').addClass('fab-header-open');
-        $('h1.fab-title, .icon-close').delay(400).fadeIn();
+        $('h1.fab-title, .close-btn').delay(400).fadeIn();
         $('.form-wrapper').delay(350).fadeIn(0);
         $('body, html').css({
             'overflow': 'hidden'
         });
     }
 
-    //fab close function
+    //fab-close-function------------------------------------------
     function fabClose() {
         $('.upload-btn').fadeIn(350);
         $('.fab').removeClass('fab-open');
         $('.overlay').fadeOut(400);
         $('.fab-header').removeClass('fab-header-open');
-        $('h1.fab-title, .icon-close, .form-wrapper').fadeOut(0);
+        $('h1.fab-title, .close-btn, .form-wrapper').fadeOut(0);
         $('.button').removeClass('f-btn-off');
         $("#form")[0].reset();
         $('body, html').css({
             'overflow': 'visible'
         });
-        $('svg.send').css({
+        $('svg.send-btn').css({
             'display': 'block'
         });
-        $('svg.done').css({
+        $('svg.done-btn').css({
             'display': 'none'
         });
         setTimeout(function() {
@@ -38,27 +38,41 @@ $(function() {
         }, 400);
     }
 
-    //fab open
+    //fab-open--------------------------------------------
     $('.fab').on('click', function() {
         fabOpen();
     });
 
-    //fab close
-    $('.overlay, .icon-close').on('click', function() {
+    //fab-close-------------------------------------------
+    $('.overlay, .close-btn').on('click', function() {
         fabClose();
     });
 
-    // contact button ani
+    //nav-menu--------------------------------------------
+    $('svg.nav-btn').on('click',function () {
+        $('.nav-bar-wrapper').toggleClass('nav-khulgya');
+        $('.overlay').fadeToggle(225);
+        $('body, html').css({
+            'overflow': 'hidden'
+        });
+    });
+    //nav-close-------------------------------------------
+    $('.overlay').on('click',function () {
+        $('.nav-bar-wrapper').removeClass('nav-khulgya');
+    });
+
+
+
+    //contact-button-ani----------------------------------
     $('.button').on('click', function() {
         $('.button').addClass('f-btn-off');
         $('.fab-header').addClass('fab-header-badlo');
         // $("#form")[0].reset();
         setTimeout(function() {
-            $('svg.send').css('display', 'none');
-            $('svg.done').css('display', 'block');
+            $('svg.send-btn').css('display', 'none');
+            $('svg.done-btn').css('display', 'block');
         }, 400);
     });
-
 
     $('.chips').on('click',function () {
         $(this).toggleClass('chip-clicked');
@@ -67,7 +81,8 @@ $(function() {
     $('.add-btn').on('click',function () {
         $(this).toggleClass('add-btn-clicked');
     });
-    //header
+
+    //header-------------------------------------------------
     $(window).scroll(function() {
         if ($(window).scrollTop() > 174) {
             $('.header').addClass('header-fixed');
@@ -76,7 +91,7 @@ $(function() {
         }
     });
 
-    //anchor for scrolling
+    //anchor-for-scrolling-------------------------------------
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
