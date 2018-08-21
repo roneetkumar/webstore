102339617141791fe9941749db1d51e4f44ac31c
Roneet Kumar <roneetparida@gmail.com>
2018-08-21 18:33:08 +0530

    minor fix

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,15 @@
+//importing data
+$.ajax({
+    url: "https://gist.githubusercontent.com/roneetkumar/0cbb9daf65e19f658236cd25c0a875bf/raw/2efef4198d153d7b6a866f5a93d35fa7a8c2199b/webstore-apps.json",
+    dataType: "json",
+    success: function(data) {
+        $.each(data.apps, function(i, item) {
+            $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
+        });
+    }
+});
+
 $(function() {
-    //importing data
-    $.ajax({
-        url: "https://gist.githubusercontent.com/roneetkumar/0cbb9daf65e19f658236cd25c0a875bf/raw/2efef4198d153d7b6a866f5a93d35fa7a8c2199b/webstore-apps.json",
-        dataType: "json",
-        success: function(data) {
-            $.each(data.apps, function(i, item) {
-                $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
-
-            });
-        }
-    });
-
     // Function-for-alphabatical-order-------------------------------
     $(function() {
         $.fn.sortList = function() {
