d562cec37f86e272dc04ce8f754ab0369a17d072
Roneet Kumar <roneetparida@gmail.com>
2018-08-22 11:32:50 +0530

    final bug fix :P

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
-//import data
-$.ajax({
-    url: "https://roneetkumar.github.io/webstore/functions/apps.json",
-    dataType: "json",
-    success: function(data) {
-        $.each(data.apps, function(i, item) {
-            $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' data-src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
-        });
-    }
-});
-
 $(function() {
+    //import data
+    $.ajax({
+        url: "https://roneetkumar.github.io/webstore/functions/apps.json",
+        dataType: "json",
+        async: false,
+        success: function(data) {
+            $.each(data.apps, function(i, item) {
+                $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' data-src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
+            });
+        }
+    });
     // Function-for-alphabatical-order-------------------------------
     $(function() {
         $.fn.sortList = function() {
@@ -214,7 +214,6 @@ $(function() {
     });
 
     $('img.app-img').Lazy();
-
 });
 
 //search--------------------------------------
