cf30cd5a4797dd84cc55a4b2859eb106667b438f
Roneet Kumar <roneetparida@gmail.com>
2018-08-22 10:33:03 +0530

    fix 2

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ $(function() {
     $.ajax({
         url: "https://roneetkumar.github.io/webstore/functions/apps.json",
         dataType: "json",
-        async: false,
         success: function(data) {
             $.each(data.apps, function(i, item) {
                 $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
